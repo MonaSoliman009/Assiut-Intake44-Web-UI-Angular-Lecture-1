eee227c5926b0ba97c9f0ada10d804b66fa90d35
MonaSoliman009 <monasoliman009@gmail.com>
2024-03-24 12:08:36 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //directive
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  // template:'<h1>From ts file</h1>',
+  // styles:[`h1{color:green}`]
 })
 export class AppComponent {
   title = 'lec1';
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Istore } from '../../models/istore';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  myStore:Istore;
+
+  constructor(){
+    this.myStore={
+      name:'H&M',
+      imgUrl:'https://fakeimg.pl/300/',
+      branches:['cairo','Assuit','Sohag']
+    }
+  }
+}
